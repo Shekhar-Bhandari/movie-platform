3e74054197cb214715262015d5954359556684a8
Shekhar Bhandari <shekharbhandari9810@gmail.com>
2025-06-03 11:22:03 +0545

    Fix case-sensitive import for Header component

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import Header from '../components/header';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 import '../index.css';
 
